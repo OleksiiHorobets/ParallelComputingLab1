951ce2df8c022f5566c4800eb4e087d140b97466
Alex Horobets <sgs.cat.fly@gmail.com>
2024-02-13 14:04:27 +0200

    added more symbols in a row

--- src/main/java/org/example/SequentialOutput.java
+++ src/main/java/org/example/SequentialOutput.java
@@ -6,7 +6,7 @@ public class SequentialOutput {
 
   private static final int SYMBOLS_AMOUNT = 100;
   private static final Object monitor = new Object();
-  private static boolean printPipe = false;
+  private static boolean printPipe = true;
 
   public static void main(String[] args) throws InterruptedException {
     Thread minusThread = new Thread(() -> printNTimes("-", SYMBOLS_AMOUNT));
@@ -15,19 +15,22 @@ public class SequentialOutput {
     minusThread.start();
     pipeThread.start();
     minusThread.join();
+    pipeThread.join();
   }
 
   @SneakyThrows
   private static void printNTimes(String str, int times) {
     for (int i = 0; i < times; i++) {
-      synchronized (monitor) {
-        while ((str.equals("-") && printPipe) || (str.equals("|") && !printPipe)) {
-          monitor.wait();
-        }
-        System.out.println(str);
-        printPipe = !printPipe;
+      for (int j = 0; j < times; j++) {
+        synchronized (monitor) {
+          while ((str.equals("-") && printPipe) || (str.equals("|") && !printPipe)) {
+            monitor.wait();
+          }
+          System.out.print(str);
+          printPipe = !printPipe;
 
-        monitor.notify();
+          monitor.notifyAll();
+        }
       }
     }
   }
